db0b30e262a0152f35596172704d5f8ed2e10642
Goncalo Leal <jobsgoncaloleal@gmail.com>
2021-06-25 22:00:10 +0100

    started preparing stacks

--- push_swap.c
+++ push_swap.c
@@ -1,32 +1,48 @@
 #include "push_swap.h"
 
-/* void prepare_stack_b(char **stack_a_args)
+int	is_input_valid(char **stack_a_args)
 {
+	int i;
 
-} */
-
-t_list *prepare_stack_a(char **stack_a_args)
-{
-	int	i;
-
+	if (!*stack_a_args)
+		return (0);
 	i = 0;
 	while (stack_a_args[i])
 	{
-		
+		if (!is_integer(stack_a_args[i]))
+			return (0);
+		i++;
 	}
+	return (1);
+}
+
+t_list *prepare_stack_a(char **stack_a_args)
+{
+	int nbr;
+	t_list	*head;
+
+	if (!is_input_valid(stack_a_args))
+		return(0);	
+	printf("\033[0;34m📌 Here in %s line %d\n\033[0m", __FILE__, __LINE__);
+	nbr = ft_atoi(stack_a_args[0]);
+	head = ft_lstnew(&nbr);
+	return (head);
+/* 	i = 0;
+	while (stack_a_args[i])
+	{
+		if (i != 0)
+	} */
 }
 
 void	push_swap(char **stack_a_args)
 {
 	t_list *a;
-	// t_list *b;
 
 	a = prepare_stack_a(stack_a_args);
-	// b = prepare_stack_b(stack_a_args);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc)
-		push_swap(argv);
+	if (argc >= 1)
+		push_swap(&argv[1]);
 }
